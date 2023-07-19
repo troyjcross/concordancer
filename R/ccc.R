3e7f3bad3563a49114bf28aaa2550cbc005222d9
troyjcross <troy.cross@sydney.edu.au>
2023-07-19 21:31:44 +1000

    Updated function documentation

--- R/ccc.R
+++ R/ccc.R
@@ -1,4 +1,10 @@
-#' An Rcpp implementation of Lin's Concordance Correlation Coefficient (CCC)
+#' @title An Rcpp implementation of Lin's Concordance Correlation Coefficient (CCC)
+#'
+#' @description
+#' This `ccc()` function leverages the speed of C++ to implement Lin's Concordance Correlation Coefficient (CCC) via [Rcpp]. This
+#' implementation is faster than other varieties of CCC calculation from other packages, i.e.,
+#' [DescTools::CCC()] and [epiR::epi.ccc()]. See
+#'
 #'
 #' @param x A numeric vector containing `x`.
 #' @param y A numeric vector containing `y`.
